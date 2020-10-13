5114c07c42176d41c2e61e9e5bc14d7bf5e048e4
unknown <khushalthepane2000@gmail.com>
2020-10-13 23:58:40 +0530

    ImagePicker

--- QuickSell/App.js
+++ QuickSell/App.js
@@ -1,6 +1,6 @@
 
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Button, Alert, Platform, StatusBar, Dimensions, TextInput } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Button, Alert, Platform, StatusBar, Dimensions, TextInput, Image } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
@@ -18,14 +18,41 @@ import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker';
 import LoginScreen from './app/screens/LoginScreen';
 import ListingEditScreen from './app/screens/ListingEditScreen';
-
+import * as ImagePicker from 'expo-image-picker'
+import * as Permissions from 'expo-permissions'
+import ImageInput from './app/components/ImageInput';
 
 
 export default function App() {
+  const [imageUri, setImageUri] = useState()
 
+  const requestPermissions = async() => {
+    const result = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION)
+    const {granted} =  await ImagePicker.requestCameraPermissionsAsync()
+    if (!granted)
+      alert('You need to enable permission to access library')
+  }
+
+  useEffect(async () => {
+    requestPermissions()
+  }, [])
+
+  const selectImage = async() => {
+    try {      
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if(!result.cancelled)
+        setImageUri(result.uri)
+    } catch (error) {
+      console.log('Error is ', error)
+    }
+  }
 
 return (
-      <ListingEditScreen />
+      <Screen>
+          <Button title="Select Image" onPress={selectImage} />
+          <Image source={{uri: imageUri }} style={{width: 200, height: 200}} />
+          <ImageInput imageUri={imageUri} />
+      </Screen>
   );
 }
 
