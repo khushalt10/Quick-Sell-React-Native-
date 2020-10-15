842b76dd5aaa0af15eba7caa760fd0a38c9f1049
unknown <khushalthepane2000@gmail.com>
2020-10-15 19:36:59 +0530

    navigation exerise

--- QuickSell/App.js
+++ QuickSell/App.js
@@ -22,13 +22,70 @@ import * as ImagePicker from 'expo-image-picker'
 import * as Permissions from 'expo-permissions'
 import ImageInput from './app/components/ImageInput';
 import ImageInputList from './app/components/ImageInputList';
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer, useNavigation } from '@react-navigation/native' 
 
+const Link = () => {
+  const navigation = useNavigation()
+
+  return(
+    <Button 
+      title="Click"
+      onPress={() => navigation.navigate("TweetDetails", { id : 1 })}
+    /> 
+  )
+}
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Link />
+  </Screen>
+)
+
+const Account = () => (
+  <Screen>
+    <Text>Accounts</Text>
+    <Link />
+  </Screen>
+)
+
+const TweetDetails = ({ route }) => (
+  <Screen>
+    <Text>Tweet Details {route.params.id}</Text>
+  </Screen>
+)
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator screenOptions={{ headerStyle: {backgroundColor: 'dodgerblue'},
+                                    headerTintColor: "white"  }}>
+    <Stack.Screen name="Tweets" component={Tweets}  />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} options={{ title: "Tweet Details" }} />
+  </Stack.Navigator>
+)
+
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator
+    tabBarOptions={{
+      activeBackgroundColor: 'tomato',
+      activeTintColor: 'white',
+      inactiveBackgroundColor: "lightgray",
+      inactiveTintColor: 'black'
+    }}
+  >
+    <Tab.Screen options={{ tabBarIcon: ({ size, color }) => <MaterialCommunityIcons color={color} name="home" size={size} />}} name="Tweets" component={StackNavigator} />
+    <Tab.Screen options={{ tabBarIcon: ({ size, color }) => <MaterialCommunityIcons color={color} name="menu" size={size} />}} name="Accounts" component={Account} />
+  </Tab.Navigator>
+)
 
 export default function App() {
-
-
 return (
-      <ListingEditScreen />
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
   );
 }
 
