464662428af3a146372c22992471694a19b866ce
unknown <khushalthepane2000@gmail.com>
2020-10-16 23:10:30 +0530

    Loading

--- QuickSell/app/screens/ListingsScreen.js
+++ QuickSell/app/screens/ListingsScreen.js
@@ -1,28 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, StyleSheet } from "react-native";
+import {  FlatList, StyleSheet } from "react-native";
 
 import routes from '../navigation/routes'
+import Button from '../components/Button'
 import Card from "../components/Card";
 import listingApi from '../api/listings' 
 import colors from "../config/colors";
 import Screen from "../components/Screen";
+import AppText from "../components/Text";
+import ActivityIndicator from "../components/ActivityIndicator";
 
 
 function ListingsScreen({navigation}) {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     loadListings();
   },[])
 
   const loadListings = async() => {
-    const res = await listingApi.getListings()
-    setListings(res.data)
+    setLoading(true);
+    const res = await listingApi.getListings();
+    setLoading(false);
+
+    if (!res.ok) return setError(true);
+
+    setError(false);
+    setListings(res.data);
   }
 
   return (
     <Screen style={styles.screen}>
-      <FlatList
+      {error && <>
+          <AppText>Couldn't retrive the listings.</AppText>
+          <Button title="Retry" onPress={loadListings} />    
+      </>}
+      <ActivityIndicator visible={true} />
+      {/* <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
         renderItem={({ item }) => (
@@ -33,7 +49,7 @@ function ListingsScreen({navigation}) {
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         )}
-      />
+      /> */}
     </Screen>
   );
 }
