637bdf28eb9dbbc3098dc4fe53d75ea4ac8c8fbf
unknown <khushalthepane2000@gmail.com>
2020-12-01 22:44:41 +0530

    cache layer

--- QuickSell/App.js
+++ QuickSell/App.js
@@ -3,11 +3,23 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import NetInfo, { useNetInfo} from '@react-native-community/netinfo'
+import { Button } from "react-native";
+import { AsyncStorage } from 'react-native';
 
 export default function App() {
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+const demo = async() => {
+  try {
+    await AsyncStorage.setItem('person', JSON.stringify({ id: 2 }))
+    const value = await AsyncStorage.getItem('person')
+    const person = JSON.stringify(value)
+    console.log(person)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+demo()
+
+  return null;
 }
