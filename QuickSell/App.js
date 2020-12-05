89437d4bff4925e184e252404ea33bdb7cb7f5a8
unknown <khushalthepane2000@gmail.com>
2020-12-05 19:33:56 +0530

    splash screen

--- QuickSell/App.js
+++ QuickSell/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import jwtDecode from 'jwt-decode'
+import { AppLoading } from 'expo'
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
@@ -11,6 +12,7 @@ import authStorage from './app/auth/storage'
 
 export default function App() {
   const [user, setUser] = useState(); 
+  const [isReady, setIsReady] = useState(false)
 
   const restoreToken = async() => {
     const token = await authStorage.getToken()
@@ -19,9 +21,8 @@ export default function App() {
 
   }
 
-  useEffect(() => {
-    restoreToken()
-  }, [])
+  if( !isReady)
+    return <AppLoading startAsync={restoreToken} onFinish={() => setIsReady(true)} />
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
